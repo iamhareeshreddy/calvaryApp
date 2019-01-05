1fddffb38632c7e0da8c8ba9bde226d11d22eacf
iamhareeshreddy <reddy.hareesh05@gmail.com>
2019-01-05 22:26:40 +0530

    Db updated

--- calvary_db.sql
+++ calvary_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3366
--- Generation Time: Dec 27, 2018 at 01:59 PM
--- Server version: 10.1.35-MariaDB
--- PHP Version: 7.2.9
+-- Host: 127.0.0.1
+-- Generation Time: Jan 05, 2019 at 05:55 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -31,11 +31,22 @@ SET time_zone = "+00:00";
 CREATE TABLE `cv_albums` (
   `id` int(11) NOT NULL,
   `album_name` varchar(255) DEFAULT NULL,
+  `album_cover` varchar(255) NOT NULL,
   `price` float DEFAULT NULL,
   `created_at` date NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '0',
   `type` int(1) NOT NULL DEFAULT '1' COMMENT '1 for audio 2 for video 3 for image'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cv_albums`
+--
+
+INSERT INTO `cv_albums` (`id`, `album_name`, `album_cover`, `price`, `created_at`, `status`, `type`) VALUES
+(1, 'first audio album', 'admin.jpg', 30, '2018-12-29', 1, 1),
+(2, 'sec', 'admin.jpg', 2, '2018-12-29', 1, 1),
+(3, 'video1 update', '1545980843beauty.jpg', 10, '2019-01-05', 1, 2),
+(4, 'imahg update', '', 12, '2019-01-05', 1, 3);
 
 -- --------------------------------------------------------
 
@@ -51,6 +62,13 @@ CREATE TABLE `cv_audio` (
   `created_at` date NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cv_audio`
+--
+
+INSERT INTO `cv_audio` (`id`, `album_id`, `audio_name`, `path`, `created_at`, `status`) VALUES
+(13, 2, 'test', 'uploads/albums/audio/2/bensound-extremeaction.mp3', '2019-01-05', 1);
 
 -- --------------------------------------------------------
 
@@ -145,19 +163,19 @@ ALTER TABLE `cv_video`
 -- AUTO_INCREMENT for table `cv_albums`
 --
 ALTER TABLE `cv_albums`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `cv_audio`
 --
 ALTER TABLE `cv_audio`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `cv_image`
 --
 ALTER TABLE `cv_image`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `cv_login`
@@ -169,7 +187,7 @@ ALTER TABLE `cv_login`
 -- AUTO_INCREMENT for table `cv_video`
 --
 ALTER TABLE `cv_video`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
